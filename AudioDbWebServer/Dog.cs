f367aa73dd725ef44ca7e74fc0bcd339e38e11cc
demond98 <demon.d.98@mail.ru>
2022-02-20 18:04:23 +0300

    Add dapper + sqlite

---
+++ AudioDbWebServer/Dog.cs
@@ -0,0 +1,8 @@
+﻿namespace AudioDbWebServer
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- AudioDbWebServer/Program.cs
+++ AudioDbWebServer/Program.cs
@@ -1,43 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.Sqlite;
+using AudioDbWebServer;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var connectString = "Data Source=C:\\Users\\Lenovo\\source\\repos\\Demond98\\AudioRepository\\AudioDbWebServer\\bin\\Debug\\net6.0\\data\\db.db";
+using var db = new SqliteConnection(connectString);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/test", async () => await db.QueryAsync<Dog>(@"SELECT * FROM Dogs"));
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
