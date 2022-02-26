a40ac77844e941fa91c60ed51425caf1c20431ad
demond98 <demon.d.98@mail.ru>
2022-02-26 23:04:55 +0300

    Добавил бд

---
+++ AudioDbWebServer/Services/SqliteService.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace AudioDbWebServer.Services
+{
+	public interface ISqliteService
+	{
+		SqliteConnection Connection { get; }
+	}
+
+	public sealed class SqliteService : IDisposable, ISqliteService
+	{
+		public SqliteConnection Connection { get; private set; }
+
+		public SqliteService(string dbPath)
+		{
+			var path = Path.GetFullPath(dbPath);
+			var connectString = "Data Source=" + Path.GetFullPath(dbPath);
+
+			if (File.Exists(path))
+				Connection = new SqliteConnection(connectString);
+			else
+			{
+				File.Create(path);
+				Connection = new SqliteConnection(connectString);
+				InitModel(Connection);
+			}
+		}
+
+		private static void InitModel(SqliteConnection connection)
+		{
+			connection.ExecuteAsync($@"
+				
+			");
+		}
+
+		public void Dispose()
+		{
+			Connection?.Dispose();
+		}
+	}
+}
